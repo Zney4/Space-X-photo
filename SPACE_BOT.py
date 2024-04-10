8697a583ffa2f6045390004be3811e505b07d53e
Zney4 <134953901+Zney4@users.noreply.github.com>
2024-04-10 18:54:35 +0300

    Update SPACE_BOT.py

--- SPACE_BOT.py
+++ SPACE_BOT.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 def photo_puclic(chat_id, time):
     while True:
         bot.send_document(
-            chat_id=chat_id, document=open(f"image/{random.choice(directory)}", "rb")
+            chat_id=chat_id, document=open(f"image/{random.choice(directory_image)}", "rb")
         )
 
         time.sleep(timer_public)
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     load_dotenv()
     bot = telegram.Bot(token=os.environ["BOT_TOKEN"])
     chat_id = os.environ["CHAT_ID"]
-    directory = os.listdir("image/")
+    directory_image = os.listdir("image/")
     timer_public = 14400
-    random_photo = random.shuffle(directory)
+    random_photo = random.shuffle(directory_image)
     photo_puclic(chat_id, time)
