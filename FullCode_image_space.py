ce189c23199ccc37bf75a4cb0e0bccde73915593
Zney4 <134953901+Zney4@users.noreply.github.com>
2024-04-26 19:08:35 +0300

    fix code

--- FullCode_image_space.py
+++ FullCode_image_space.py
@@ -4,7 +4,7 @@ import datetime
 from dotenv import load_dotenv
 
 
-def donload_image(filename, url):
+def download_image(filename, url):
     response = requests.get(url)
     response.raise_for_status()
 
@@ -80,32 +80,32 @@ def donload_image_APOUD_nasa(day_image, file_format):
     response = requests.get(day_image)
     response.raise_for_status()
 
-    with open(f"image/ Astronomy Picture of the Day{file_f}", "wb") as file:
+    with open(f"image/ Astronomy Picture of the Day{file_format}", "wb") as file:
         file.write(response.content)
 
 
 def breaks_file(day_image):
     image_format = os.path.splitext(day_image)
 
-    return image_f[1]
+    return image_format[1]
 
 
 if __name__ == "__main__":
     load_dotenv()
 
     os.makedirs("image", mode=0o777, exist_ok=True)
-    
+
     id = "5eb87d47ffd86e000604b38a"
     filename = "1.jpeg"
     url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
-    donload_image(filename, url)
+    download_image(filename, url)
 
     space_photo_links = get_photo(id)
     filename_space = "spacex.jpg"
     fetch_spacex_last_launch(space_photo_links, filename_space)
 
     api_key = os.environ["API_KEY"]
-    day_image = nasa_APOD(api_key)
+    day_image = APOD_nasa(api_key)
     epic_image = nasa_EPIC(api_key)
     file_format = breaks_file(day_image)
     donload_image_APOUD_nasa(day_image, file_format)
