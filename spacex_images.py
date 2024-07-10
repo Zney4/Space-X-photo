2472a9f60a1760b90243105a7150ad2508412594
Zney4 <134953901+Zney4@users.noreply.github.com>
2024-07-10 18:37:45 +0300

    fix code

--- spacex_images.py
+++ spacex_images.py
@@ -2,13 +2,7 @@ from dowload_image_space import download_images_from_urls
 from dowload_image_space import get_request
 
 
-def get_photos(id):
-    spacex_photos = get_request(f"https://api.spacexdata.com/v5/launches/{id}", None)["links"]["flickr"]["original"]
-
-    return spacex_photos
-
-
 if __name__ == "__main__":
     id = "5eb87d47ffd86e000604b38a"
-    space_photo_links = get_photos(id)
-    download_images_from_urls(urls=space_photo_links, file_name_prefix="space")
+    spacex_photos = get_request(f"https://api.spacexdata.com/v5/launches/{id}", None)["links"]["flickr"]["original"]
+    download_images_from_urls(urls=spacex_photos, file_name_prefix="space")
